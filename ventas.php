8043f05542cc79082327d9e6366b451cd5c424f3
Erick Alexander Chávez González <201800153@colsanjose.edu.gt>
2020-05-14 14:53:52 -0600

    ESTETICA

--- ventas.php
+++ ventas.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
     <head>
 	 <link rel="stylesheet" type="text/css" href="Estilos.css" />
 	 <meta charset="utf-8">
@@ -12,7 +12,9 @@
 		  <h1>VENTAS</h1>
 	    </div>
 		<div align="right">
-            <a href="index.html">SALIR</a>
+		    <button type="button" class="btn btn-light">
+			   <a href="index.html">SALIR</a>
+			</button>      
         </div>
 		<?php
             if(isset($_GET['producto']))
@@ -64,43 +66,148 @@
           <div class="carousel-inner">
              <div class="carousel-item active">
                <img src="imagenes/Spider-Man.png" class="d-block w-25">
-               <div class="text-white">
-                 <h5>SPIDER-MAN</h5>
-                 <p>Q600</p>
-				 <p><a href="?producto=SPIDEY&precio=600">COMPRAR</a></p>
-               </div>
+			   <div class="text-black">
+			         <h5>
+					    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#Spidey">
+						  SPIDER-MAN
+						  Q600					 
+						</button>
+						<div class="modal fade" id="Spidey" tabindex="-1" role="dialog" aria-labelledby="Spidey" aria-hidden="true">
+						  <div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+							  <div class="modal-header">
+								<h5 class="modal-title" id="Spidey">SPIDER-MAN</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								  <span aria-hidden="true">&times;</span>
+								</button>
+							  </div>
+							  <div class="modal-body">
+								Es un videojuego de accion y aventuras con un estilo jugable de mundo abierto, que permite al jugador desplazarse de un lugar a otro con total libertad de movimientos y que tiene por escenario donde de desarrolla la historia, la ciudad de Nueva York.
+							  </div>							  
+							</div>
+						  </div>
+						</div>  
+				     </h5>	 
+			   </div>		  
+               <button type="button" class="btn btn-light">
+			     <p><a href="?producto=SPIDEY&precio=600">COMPRAR</a></p>
+			   </button>  
              </div>
              <div class="carousel-item">
                <img src="imagenes/God.jpg" class="d-block w-25">
-                 <div class="text-white">
-                   <h5>GOD OF WAR</h5>
-				   <p>Q250</p>
-                   <p><a href="?producto=GOD&precio=250">COMPRAR</a></p>
-                 </div>
+			   <div class="text-black">
+			         <h5>
+					    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#God">
+						  GOD OF WAR
+						  Q250
+						</button>
+						<div class="modal fade" id="God" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						  <div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+							  <div class="modal-header">
+								<h5 class="modal-title" id="exampleModalCenterTitle">GOD OF WAR</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								  <span aria-hidden="true">&times;</span>
+								</button>
+							  </div>
+							  <div class="modal-body">
+								Es un videojuego de accion-aventura desarrollado por SCE Santa Monica Studio y publicado por Sony Interactive Entertainment. Su lanzamiento se produjo el 20 de abril de 2018, en exclusiva para la consola PlayStation 4.​
+							  </div>							  
+							</div>
+						  </div>
+						</div>  
+				     </h5>
+			   </div>		  
+                 <button type="button" class="btn btn-light">
+			     <p><a href="?producto=GOD&precio=250">COMPRAR</a></p>
+			   </button>  
              </div>
           <div class="carousel-item">
             <img src="imagenes/Red.jpg" class="d-block w-25">
-            <div class="text-white">
-              <h5>RED DEAD REDEMPTION</h5>
-              <p>Q450</p>
-			  <p><a href="?producto=RED&precio=450">COMPRAR</a></p>
-            </div>
+			<div class="text-black">
+			         <h5>
+					    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#Red">
+						  RED DEAD REDEMPTION 2
+						  Q450
+						</button>
+						<div class="modal fade" id="Red" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						  <div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+							  <div class="modal-header">
+								<h5 class="modal-title" id="exampleModalCenterTitle">RED DEAD REDEMPTION 2</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								  <span aria-hidden="true">&times;</span>
+								</button>
+							  </div>
+							  <div class="modal-body">
+								Es un videojuego de accion-aventura western, en un mundo abierto y en perspectiva de primera y tercera persona, ​ con componentes para un jugador y multijugador.​ Fue desarrollado por Rockstar Studios. Es la precuela de Red Dead Redemption y el tercer juego de la saga Red Dead.
+							  </div>							  
+							</div>
+						  </div>
+						</div>  
+				     </h5>
+			   </div>		  
+             <button type="button" class="btn btn-light">
+			     <p><a href="?producto=RED&precio=450">COMPRAR</a></p>
+			 </button>  
           </div>
 		  <div class="carousel-item">
             <img src="imagenes/Rock.jpg" class="d-block w-25">
-            <div class="text-white">
-              <h5>ROCKET LEAGUE</h5>
-              <p>Q150</p>
-			  <p><a href="?producto=ROCKET&precio=150">COMPRAR</a></p>
-            </div>
+			<div class="text-black">
+			         <h5>
+					    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#Rock">
+						  ROCKET LEAGUE
+						  Q150
+						</button>
+						<div class="modal fade" id="Rock" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						  <div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+							  <div class="modal-header">
+								<h5 class="modal-title" id="exampleModalCenterTitle">ROCKET LEAGUE</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								  <span aria-hidden="true">&times;</span>
+								</button>
+							  </div>
+							  <div class="modal-body">
+								Es un videojuego que combina el futbol con los vehículos. Fue desarrollado por Psyonix y lanzado el 7 de julio del 2015. Se encuentra disponible en español, tiene modos de juego cooperativo, de un jugador y en línea. Está disponible para PC, PlayStation 4, Xbox One y Nintendo Switch.
+							  </div>							  
+							</div>
+						  </div>
+						</div>  
+				     </h5>
+			   </div>		  
+             <button type="button" class="btn btn-light">
+			     <p><a href="?producto=ROCKET&precio=150">COMPRAR</a></p>
+			   </button>  
           </div>
 		  <div class="carousel-item">
             <img src="imagenes/Mine.png" class="d-block w-25">
-            <div class="text-white">
-              <h5>MINECRAFT</h5>
-              <p>Q250</p>
-			  <p><a href="?producto=MINE&precio=250">COMPRAR</a></p>
-            </div>
+			<div class="text-black">
+			         <h5>
+					    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#Mine">
+						  MINECRAFT
+						  Q250
+						</button>
+						<div class="modal fade" id="Mine" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						  <div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+							  <div class="modal-header">
+								<h5 class="modal-title" id="exampleModalCenterTitle">MINECRAFT</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								  <span aria-hidden="true">&times;</span>
+								</button>
+							  </div>
+							  <div class="modal-body">
+								Es un videojuego de construcción, de tipo «mundo abierto» o sandbox creado originalmente por el sueco Markus Persson, ​ y posteriormente desarrollado por su empresa, Mojang AB.
+							  </div>							  
+							</div>
+						  </div>
+						</div>  
+				     </h5>
+			   </div>		  
+             <button type="button" class="btn btn-light">
+			     <p><a href="?producto=MINE&precio=250">COMPRAR</a></p>
+			 </button>  
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
